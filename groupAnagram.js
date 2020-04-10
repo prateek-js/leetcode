9e2fd7457b4fa46bbf1216433c3f2bd290006604
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-10 20:52:05 +0530

    Create groupAnagram.js

---
+++ groupAnagram.js
@@ -0,0 +1,18 @@
+var groupAnagrams = function(strs) {
+  let map = new Map();
+    for (let str of strs) {
+        console.log(str);
+        const s = uniform(str);
+        let l = map.get(s) || [];
+        l.push(str);
+        map.set(s, l);
+    }
+    return Array.from(map.values());
+};
+
+var uniform = function(str) {
+    return str
+        .split("")
+        .sort()
+        .join("");
+};
