cb0e23ff20b21bb220a954d88c0f47f412d5801a
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-10 20:50:06 +0530

    Create bckspaceStrCompare.js

---
+++ bckspaceStrCompare.js
@@ -0,0 +1,19 @@
+var backspaceCompare = function(S, T) {
+    return edit(S) === edit(T); 
+}
+
+var edit = function(str) {
+        let result = '';
+        let backspaces = 0;
+        
+        for (let i = str.length - 1; i >= 0; i -= 1) {        
+            if (str[i] === '#') {
+                backspaces += 1;
+            } else if (backspaces > 0) {
+                backspaces -= 1;
+            } else {
+                result = str[i] + result;
+            }
+        }
+        return result;
+}
