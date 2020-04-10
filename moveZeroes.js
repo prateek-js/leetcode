df8da3def491726cc73e1d91d39bca620bc4e184
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-10 20:53:42 +0530

    Create moveZeroes.js

---
+++ moveZeroes.js
@@ -0,0 +1,15 @@
+ar moveZeroes = function(nums) {
+    let count = 0;
+    let len = nums.length;
+    for (let i = 0; i < len; i++) 
+        if (nums[i] != 0) 
+            nums[count++] = nums[i]; // here count is  
+                                   // incremented 
+  
+    // Now all non-zero elements have been shifted to  
+    // front and  'count' is set as index of first 0.  
+    // Make all elements 0 from count to end. 
+    for (let j = count; j < len; j++) {
+        nums[j] = 0;
+    }
+};
