25bf83787587b74fecff58da672a13f3d0b1d2bc
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-10 20:44:30 +0530

    Create mediansortedArray.js

---
+++ mediansortedArray.js
@@ -0,0 +1,10 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    let x = nums1.concat(nums2);
+    x.sort(function (a,b) {
+        return a - b;
+    });
+    let len = x.length;
+  
+    return len%2 === 0 ? (x[Math.floor(len/2)-1] + x[Math.ceil(len/2)])/2 : x[Math.floor(len/2)];
+    
+};
