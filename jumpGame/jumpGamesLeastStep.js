9cb806607d3f5a4a40357b70df32e782197f8ec7
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-25 21:00:30 +0530

    Update jumpGamesLeastStep.js

--- jumpGame/jumpGamesLeastStep.js
+++ jumpGame/jumpGamesLeastStep.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+//O(n*n)
 var jump = function(nums) {
     let post = nums.length - 1;
     let steps = 0;
@@ -15,4 +16,23 @@ var jump = function(nums) {
         }
     }
     return steps;
+};
+
+// O(n)
+var jump = function(nums) {
+    let ladder = nums[0];
+    let stairs = nums[0];
+    let jumps = 1;
+       for(let i = 1 ; i < nums.length; i++) {
+            if(i == nums.length-1) return jumps;
+            if(i + nums[i] > ladder) {
+                ladder = i + nums[i];
+            }
+            stairs--;
+            if(stairs == 0) {
+                jumps++;
+                stairs = ladder - i;
+            }
+        }
+        return 0;
 };
