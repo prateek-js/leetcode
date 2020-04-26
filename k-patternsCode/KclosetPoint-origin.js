bf2b7945c3269d4e25fba23a07b481b8b30bc8f0
Prateek Desai <prateekdesai1990@gmail.com>
2020-04-27 00:54:12 +0530

    Update KclosetPoint-origin.js

--- k-patternsCode/KclosetPoint-origin.js
+++ k-patternsCode/KclosetPoint-origin.js
@@ -3,6 +3,16 @@
  * @param {number} K
  * @return {number[][]}
  */
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+
+
 var kClosest = function(points, K) {
     
     var swap = (p1,p2) => {
